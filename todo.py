49882465e069608ef0ff325d31c1fe544de2074b
MichaelW0607 <113442619+MichaelW0607@users.noreply.github.com>
2023-03-09 10:51:04 -0500

    jinja 2

--- todo.py
+++ todo.py
@@ -13,7 +13,7 @@ connection = pymysql.connect(
     host="10.100.33.60",
     user="mwilliams",
     password="220467419",
-    database="world",
+    database="mwilliams_Todos",
     cursorclass=pymysql.cursors.DictCursor,
     autocommit=True
 )
@@ -21,11 +21,30 @@ connection = pymysql.connect(
 @app.route("/")
 def index():
   cursor = connection.cursor()
-  cursor.execute("SELECT")
+  cursor.execute("SELECT * FROM `Todos`" )
+  cursor.execute("SELECT * FROM `Todos` ORDER BY `Complete`")
   results = cursor.fetchall()
-  return render_template("todo.html.jinja",todos ="My_todo")
+  return render_template("todo.html.jinja",
+  My_todo=results
+  )
+
+
 
 @app.route("/add", methods = ["POST"])
 def add ():
   new_todo = request.form["new_todo"]
+
+   
+
+  cursor.execute(f"INSERT INTO `Todos`(`Description`) VALUES ('{new_todo}') ")
+
+  my_todo.append(new_todo)
   return new_todo
+
+cursor = connection.cursor()
+
+cursor.execute("SELECT * FROM `Todos` ")
+
+result = cursor.fetchall()
+
+print(result)
